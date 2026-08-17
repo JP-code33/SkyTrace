f85bc955ba68e3c85490efa157eb0949278ddeb6
JP-code33 <pateljhenil@gmail.com>
2026-08-16 21:26:37 -0400

    tried many new API but didn't work so by not wasting a lot of time I started on making new things. I have made a map style choosing system.

--- skytrace/src/main.js
+++ skytrace/src/main.js
@@ -2,7 +2,34 @@ import "leaflet/dist/leaflet.css"
 import "./style.css"
 import L from "leaflet"
 
-const map = L.map("map").setView([39.8, -98.5], 4)
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+const skyTraceMap = L.map("map").setView([39.8, -98.5], 4)
+
+const skyTraceStandardMap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreeMap contributors"
-}).addTo(map)
+})
+
+const skyTraceDarkLayer = L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  attribution: "&copy; OpenStreetMap contributors &copy; CARTO"
+})
+
+const skyTraceSatelliteLayer = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
+  attribution: "Tiles &copy; Esri"
+})
+
+const skyTraceTerrainLayer = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap contributors, SRTM | OpenTopoMap"
+})
+
+skyTraceStandardMap.addTo(skyTraceMap)
+
+const skyTraceMapLayers = {standard: skyTraceStandardMap, dark: skyTraceDarkLayer, satellite: skyTraceSatelliteLayer, terrain: skyTraceTerrainLayer}
+
+function changeSkyTraceMapLayer(skyTraceLayerName) {
+  Object.values(skyTraceMapLayers).forEach((skyTraceLayer) => {
+    if(skyTraceMap.hasLayer(skyTraceLayer)) {
+      skyTraceMap.removeLayer(skyTraceLayer)
+    }
+  })
+  skyTraceMapLayers[skyTraceLayerName].addTo(skyTraceMap)
+}
+window.changeSkyTraceMapLayer = changeSkyTraceMapLayer
