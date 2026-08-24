2ba9d99a6733391efed8b4aeb0aaaeba2970b47b
JP-code33 <pateljhenil@gmail.com>
2026-08-24 11:23:12 -0400

    added another API for the route for the flights

---
+++ skytrace/api/route.js
@@ -0,0 +1,39 @@
+export default async function handler(request, response) {
+    try {
+        const callsign = request.query.callsign?.trim()
+        if(!callsign) {
+            return response.status(400).json({
+                error: "Missing callsign"
+            })
+        }
+
+        const routeURL = `https://api.adsbdb.com/v0/callsign/${encodeURIComponent(callsign)}`
+        const routeResponse = await fetch(routeUrl)
+
+        if(!routeResponse.ok) {
+            return response.status(routeResponse.status).json({
+                error: "ADSBDB route lookup failed"
+            })
+        }
+
+        const data = await routeResponse.json()
+        const route = data.response?.flightroute
+
+        if(!reoute) {
+            return response.status(400).json({
+                error: "Route not found"
+            })
+        }
+
+        return response.status(200).json({
+            airline: route.airline?.name || null,
+            origin: route.origin ? {name: route.origin.name, iata: route.origin.iata_code, icao: route.origin.icao_code} : null,
+            destination : route.destination ? {name: route.destination.name, iata: route.destination.iata_code, icao: route.destination.icao_code} : null
+        })
+    } catch(error) {
+        console.error(error)
+        return response.status(500).json({
+            error: "Failed to fetch route data"
+        })
+    }
+}
