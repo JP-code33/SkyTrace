dc1f821171bb7060e988fe0f4aa7eb83525faa34
JP-code33 <pateljhenil@gmail.com>
2026-08-26 08:41:24 -0400

    fixed another issue

--- skytrace/src/main.js
+++ skytrace/src/main.js
@@ -156,7 +156,7 @@ const skyTraceReplayCurrentTime = document.getElementById("skyTraceReplayCurrent
 const skyTraceReplayTotalTime = document.getElementById("skyTraceReplayTotalTime")
 const skyTraceReplayPlayPause = document.getElementById("skyTraceReplayPlayPause")
 const skyTraceReplayRestart = document.getElementById("skyTraceReplayRestart")
-const skyTraceReplaySpeedButtons = document.querySelectorAll("#skyTraceReplaySpeedButtons buttons")
+const skyTraceReplaySpeedButtons = document.querySelectorAll(".skyTraceReplaySpeed")
 
 function createAircraftIcon(heading) {
   return L.divIcon({
